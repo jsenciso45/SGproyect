e652b791009b8a50112aeb64811847082cc5fc14
Juan Enciso <sebas.enciso121@gmail.com>
2021-10-08 15:55:47 -0500

    Portafolio definitivo V.1.3

---
+++ portafolio definitivo/Ejercicios/indexEjer.php
@@ -0,0 +1,69 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <title>EJERCICIOS JAVASCRIPT</title>
+    <link rel="stylesheet" href="../css/crud.css">
+</head>
+<body>
+<section class="portfolio" id="portfolio">
+
+    <h1 class="heading"> Mi <span>Portafolio</span> </h1>
+
+    <div class="box-container">
+
+        <div class="box">
+            <img src="../img/rojo.jpg" alt="">
+            <div class="content">
+                <a href="../crud/indexCrud.php"><h3>Crud</h3></a>
+            </div>
+        </div>
+
+        <div class="box">
+            <img src="../img/rojo.jpg" alt="">
+            <div class="content">
+                <h3>JavaScript</h3>
+            </div>
+        </div>
+
+        <div class="box">
+            <img src="../img/rojo.jpg" alt="">
+            <div class="content">
+                <h3>Manuales De Usualio</h3>
+            </div>
+        </div>
+    </div>
+
+</section>
+<section class="portfolio" id="portfolio">
+
+    <h1 class="heading"> Mi <span>Portafolio</span> </h1>
+
+    <div class="box-container">
+
+        <div class="box">
+            <img src="../img/rojo.jpg" alt="">
+            <div class="content">
+                <a href="../crud/indexCrud.php"><h3>Crud</h3></a>
+            </div>
+        </div>
+
+        <div class="box">
+            <img src="../img/rojo.jpg" alt="">
+            <div class="content">
+                <h3>JavaScript</h3>
+            </div>
+        </div>
+
+        <div class="box">
+            <img src="../img/rojo.jpg" alt="">
+            <div class="content">
+                <h3>Manuales De Usualio</h3>
+            </div>
+        </div>
+    </div>
+
+</section>
+    
+</body>
+</html>
