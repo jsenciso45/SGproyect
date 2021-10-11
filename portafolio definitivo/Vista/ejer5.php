1a7d447276038a99c36c97bca7dbc80ea863d9f6
jsenciso <sebas.enciso121@gmail.com>
2021-10-11 09:04:06 -0500

    ULTIMO COMMIT!!

--- portafolio definitivo/Vista/ejer5.php
+++ portafolio definitivo/Vista/ejer5.php
@@ -7,82 +7,33 @@
 <body>
         <pre>
             
-            // declaracion de variables
-            var sumar=()=>{
-            var n1 = document.getElementById('txtN1').value;
-            var n2 = document.getElementById('txtN2').value;
-            var suma = parseInt(n1) + parseInt(n2);
-            alert('la suma de los digitos '+ n1 + ' y '+ n2 +' es '+suma);
-            }
-
-            var resta=()=>{
-            var n1 = document.getElementById('txtN1').value;
-            var n2 = document.getElementById('txtN2').value;
-            var restar = parseInt(n1) - parseInt(n2);
-            alert('la resta de los digitos '+ n1 + ' y '+ n2 +' es '+restar);
-             }
-
-            var mayor=()=>{
-            var n1 = document.getElementById('txtN1').value;
-            var n2 = document.getElementById('txtN2').value;
-
-            if (n1>=n2) {
-            alert(n1 + 'es mayor que '+n2);        
-            } else {
-            alert(n2 + 'es mayor que '+n1);
-            }
-             }
-
-                var menor=()=>{
-                    var n1 = document.getElementById('txtN1').value;
-                    var n2 = document.getElementById('txtN2').value;
-
-                    if (n1<=n2) {
-                        alert(n1 + 'es menor que '+n2);        
-                    } else {
-                        alert(n2 + 'es menor que '+n1);
-                    }
-                }
-
-                var mediana=()=>{
-                    var strNum=prompt("Ingrese numeros separados con comas");
-                    var nums=strNum.split(",");
-
-                    for(i=0, length=nums.length;i<length;i++){
-                        nums[i]=parseInt(nums[i]);
-                    }
-                }
-
-                var OptMath=()=>{
-                    var cond=document.getElementById('select');
-                    var eval=cond.value;
-
-                    if (eval=="suma") {
-                        sumar();
-                    }else{
-                        if (eval=="resta") {
-                            resta();
-                        } else {
-                            if (eval=="mayor") {
-                                mayor();                
-                            } else {
-                                if (eval=="menor") {
-                                    menor()
-                                } else {
-
-                                }
-
-                            }
-
-                        }
-                    }
-
-                }
- 
-
-
-
-
+        function llenarVector(vector, tam) {
+        for (let i = 0; i < tam; i++) {
+        vector.push(Math.round(Math.random() * 10));
+        }
+        return vector;
+        }
+    
+        var llenar = (vector, tam) => {
+        for (let i = 0; i < tam; i++) {
+        vector.push(Math.round(Math.random() * 10));
+        }
+        return vector;
+        };
+        v = new Array();
+        
+        console.log(llenar(v,2));
+    
+        var suma = (vector) => {
+        var s = 0;
+        for (let i = 0; i < vector.length; i++) {
+        s = s + vector[i];
+        
+        }
+        return s;
+    
+        }
+        console.log("la Suma de los vectores es: " + suma(v));
 
         </pre>
     
