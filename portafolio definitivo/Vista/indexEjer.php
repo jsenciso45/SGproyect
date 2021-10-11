a0c2ae294d6d7ab018f1912ae6188a4d057ae203
jsenciso <sebas.enciso121@gmail.com>
2021-10-11 00:53:17 -0500

    cambios-5

--- portafolio definitivo/Vista/indexEjer.php
+++ portafolio definitivo/Vista/indexEjer.php
@@ -6,44 +6,44 @@
     <link rel="stylesheet" href="../css/Crud-2.css">
 </head>
 <body>
+
 <section class="portfolio" id="portfolio">
 
-    <h1 class="heading"> Java <span>Script</span> </h1>
-
+<h1 class="heading"> Java <span>Script</span> </h1>
     <div class="box-container">
 
         <div class="box">
-            <img src="../img/rojo.jpg" alt="">
+            <img src="../img/js.jpg" alt="">
             <div class="content">
-                <a href="../crud/indexCrud.php"><h3>Crud</h3></a>
+                <a href="../crud/indexCrud.php"><h3>EJERCICIO 1</h3></a>
             </div>
         </div>
 
         <div class="box">
-            <img src="../img/rojo.jpg" alt="">
+        <img src="../img/js.jpg" alt="">
             <div class="content">
-                <h3>JavaScript</h3>
+            <a href="../crud/indexCrud.php"><h3>EJERCICIO 2</h3></a>
             </div>
         </div>
 
         <div class="box">
-            <img src="../img/rojo.jpg" alt="">
+        <img src="../img/js.jpg" alt="">
             <div class="content">
-                <h3>Manuales De Usualio</h3>
+            <a href="../crud/indexCrud.php"><h3>EJERCICIO 3</h3></a>
             </div>
         </div>
 
         <div class="box">
-            <img src="../img/rojo.jpg" alt="">
+        <img src="../img/js.jpg" alt="">
             <div class="content">
-                <h3>Manuales De Usualio</h3>
+            <a href="../crud/indexCrud.php"><h3>EJERCICIO 4</h3></a>
             </div>
         </div>
 
         <div class="box">
-            <img src="../img/rojo.jpg" alt="">
+        <img src="../img/js.jpg" alt="">
             <div class="content">
-                <h3>Manuales De Usualio</h3>
+            <a href="../crud/indexCrud.php"><h3>EJERCICIO 5</h3></a>
             </div>
         </div>
     </div>
